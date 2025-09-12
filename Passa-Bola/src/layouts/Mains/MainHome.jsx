37eb58ad962c32a4b479fa8b917e9c07b13ec17c
Pedro Marchese <rm563339@fiap.com.br>
2025-09-12 12:53:50 -0300

    chore: jogos inscritos

--- Passa-Bola/src/layouts/Mains/MainHome.jsx
+++ Passa-Bola/src/layouts/Mains/MainHome.jsx
@@ -4,7 +4,6 @@ const MainHome = () => {
   return (
     <main>
         <h1 className='text-white'>Jogos Inscritos</h1>
-        <ItemsCarousel size = {5} storageKey="jogosInscritos" textColor='text-white' type = "jogos inscritos"/>
     </main>
     
   )
