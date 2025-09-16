047667e76a6b63e9f76cb55f4a4b35b255d848e6
Pedro Marchese <rm563339@fiap.com.br>
2025-09-16 08:58:57 -0300

    refactor: melhorando o código

--- Passa-Bola/src/layouts/Mains/MainLogin.jsx
+++ Passa-Bola/src/layouts/Mains/MainLogin.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
--- Passa-Bola/src/pages/childrens-pages/Analytics.jsx
+++ Passa-Bola/src/pages/childrens-pages/Analytics.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import {
